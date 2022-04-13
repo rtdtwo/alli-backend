46095f2ab579aa75f2f2bd87c41ce8082fa538be
Rishabh Tatiraju <me@rishabh.blog>
2022-04-13 17:49:02 -0400

    Added CORS

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import bl
 import waitress
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/health_check')
 def health_check():
